7a34e94f1283c0cead03dbe8bc26e39673010c9a
Paul Joubert <paul@trespaul.com>
2025-03-30 20:21:52 +0200

    add solidjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anmarivanderwesthuizen.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), solidJs({devtools: true})],
   image: {
     domains: ["anmari.trespaul.com"],
   },
