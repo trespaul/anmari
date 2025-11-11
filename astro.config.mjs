f1bb93b48e55fc433c424022adb2863293472266
Paul Joubert <paul@trespaul.com>
2025-11-11 15:44:25 +0200

    upgrade to tailwind 4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,18 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anmarivanderwesthuizen.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
+
   image: {
     domains: ["anmari.trespaul.com"],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
